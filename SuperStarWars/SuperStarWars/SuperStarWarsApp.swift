80b27238f7155ced66a452e6afc72bd49442336e
enchufaocoding <enchufaocoding@gmail.com>
2025-12-11 06:06:38 +0100

    Create Project and setup initial values
    Create Project and setup initial values
    first commit to github

---
+++ SuperStarWars/SuperStarWars/SuperStarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperStarWarsApp.swift
+//  SuperStarWars
+//
+//  Created by Jose Alberto Rosario Castillo on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SuperStarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
